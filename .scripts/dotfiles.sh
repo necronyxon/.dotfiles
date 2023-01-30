446e29f2c433def30db64bb57f5259b80293f571
Rafa Herzog <rafael.oliveira@mjv.com.br>
2023-01-30 11:37:02 -0300

    fix script

--- .scripts/dotfiles.sh
+++ .scripts/dotfiles.sh
@@ -1,17 +1,34 @@
+#!/bin/bash
+
 git clone --bare https://github.com/necronyxon/.dotfiles.git $HOME/.dotfiles
 
 function dotfiles {
-   /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
+  /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
 }
 
 mkdir -p .config-backup
 
-dotfiles checkout
+dotfiles checkout 
 if [ $? = 0 ]; then
-  echo "Checked out config.";
-  else
-    echo "Backing up pre-existing dotfiles.";
-    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+  echo "------------------------------------------------------------"
+  echo "Checked out config";
+  echo "------------------------------------------------------------"
+else
+  echo "------------------------------------------------------------"
+  echo "Backing up pre-existing dotfiles...";
+  echo "------------------------------------------------------------"
+
+  current_location=`dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'}` 
+  echo "$current_location" | while read file; do
+    backup_file=`echo "$file" | sed 's#^#.config-backup/#'`
+
+    [ -d "$file" ] || mkdir -p `echo "$backup_file" | grep '.*\/' -o`
+
+    echo "Moving $file to $backup_file"
+    mv "$file" "$backup_file"
+  done 
+
+  echo "------------------------------------------------------------"
 fi;
 dotfiles checkout
 dotfiles config --local status.showUntrackedFiles no
